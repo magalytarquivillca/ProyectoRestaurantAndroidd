87b34d182ae91b10a665b00bb709e462c054c60d
Rodrigo Bejarano <rodribej.2@gmail.com>
2020-10-13 21:29:24 -0400

    añadiendo interfaces crear restaurant, hacer orden y seleccionar lugar

---
+++ app/src/main/java/com/seminario/proyectorestaurantandroid/make_order_1.java
@@ -0,0 +1,14 @@
+package com.seminario.proyectorestaurantandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class make_order_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_order_1);
+    }
+}
